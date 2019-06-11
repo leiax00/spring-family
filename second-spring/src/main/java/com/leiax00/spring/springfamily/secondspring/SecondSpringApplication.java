2c6a098a452b4a5cda9b2ac0086148cc19fcb42a
Leiax00 <leiax00@outlook.com>
2019-06-11 22:50:30 +0800

    demo: configure datasource

---
+++ second-spring/src/main/java/com/leiax00/spring/springfamily/secondspring/SecondSpringApplication.java
@@ -0,0 +1,46 @@
+package com.leiax00.spring.springfamily.secondspring;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@SpringBootApplication
+@Slf4j
+public class SecondSpringApplication implements CommandLineRunner {
+    private final DataSource dataSource;
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public SecondSpringApplication(DataSource dataSource, JdbcTemplate jdbcTemplate) {
+        this.dataSource = dataSource;
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecondSpringApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        showConnection();
+        showData();
+    }
+
+    private void showConnection() throws SQLException {
+        log.info(dataSource.toString());
+        Connection conn = dataSource.getConnection();
+        log.info("conn string: " + conn.toString());
+        conn.close();
+    }
+
+    private void showData() {
+        jdbcTemplate.queryForList("SELECT * FROM FOO")
+                .forEach(row -> log.info(row.toString()));
+    }
+}
